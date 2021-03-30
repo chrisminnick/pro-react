2159317f5bf929b8460e1339ff962d0a9145804b
Chris Minnick <chris@minnick.com>
2021-03-30 00:47:20 -0700

    lab13

---
+++ new-solutions/lab12/README.md
@@ -0,0 +1,30 @@
+# React Project Template
+
+React Project Template with automated build.
+
+## Installation
+
+1. Install Node.js
+2. Install git.
+3. In the console, type: npm install
+
+## Usage
+To build:
+
+1. npm run build
+
+To start:
+
+1. npm start
+
+To lint:
+
+1. npm run lint
+
+To test:
+
+1. npm run test
+
+## Credits
+
+## License
---
+++ new-solutions/lab12/src/sayHello.test.js
@@ -0,0 +1,29 @@
+var sayHello = require('../src/scripts/sayHello.js');
+
+describe('Greet', function() {
+    test('concats Hello and a name', function() {
+        var actual = sayHello.greet('World');
+        var expected = 'Hello, World';
+        expect(actual).toEqual(expected);
+    });
+
+    test('says "Hello, Friend!" when no name is given', function() {
+        var actual = sayHello.greet('');
+        var expected = 'Hello, Friend!';
+        expect(actual).toEqual(expected);
+    });
+
+    test('says good morning in the morning', function() {
+        var theDate = new Date('April 01, 2021 08:00:00');
+
+        var actual = sayHello.greet('World',theDate);
+        var expected = 'Good Morning, World';
+        expect(actual).toEqual(expected);
+
+    });
+    test('speaks German to Germans', function() {
+        var actual = sayHello.greet('Hans');
+        var expected = 'Guten Tag, Hans';
+        expect(actual).toEqual(expected);
+    });
+});
